2d5b9b933cfe65f53a18a95b9fa0fd03624215df
Ole Magnus Fon Johnsen <me@omfj.no>
2025-04-22 08:41:06 +0200

    Reset terraform

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -30,3 +30,57 @@ resource "azurerm_application_insights" "axis_ai" {
   application_type    = "other"
   workspace_id        = azurerm_log_analytics_workspace.axis_log_ws.id
 }
+
+resource "azurerm_monitor_diagnostic_setting" "vertex_diagnostics" {
+  name                       = "vertex-diagnostics"
+  target_resource_id         = azurerm_linux_web_app.echo_vertex.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.vertex_log_ws.id
+
+  enabled_log {
+    category = "AppServiceAppLogs"
+  }
+
+  enabled_log {
+    category = "AppServiceConsoleLogs"
+  }
+
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+  }
+
+  enabled_log {
+    category = "AppServicePlatformLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "axis_diagnostics" {
+  name                       = "axis-diagnostics"
+  target_resource_id         = azurerm_linux_web_app.echo_axis.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.axis_log_ws.id
+
+  enabled_log {
+    category = "AppServiceAppLogs"
+  }
+
+  enabled_log {
+    category = "AppServiceConsoleLogs"
+  }
+
+  enabled_log {
+    category = "AppServiceHTTPLogs"
+  }
+
+  enabled_log {
+    category = "AppServicePlatformLogs"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,11 @@ terraform {
 provider "azurerm" {
   subscription_id = var.subscription_id
 
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "azurerm_resource_group" "rg" {
